1a7ea558a1a1fb336f56bf0cba51c36376d39c3a
danielerat <davidodo2015@gmail.com>
2023-03-30 19:06:44 +0200

    Creating our custom User

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+from django.db import models
+from django.core.validators import RegexValidator
+
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+from core.user_manager import CustomUserManager
+
+# Custom User Model that is Only Unique to this project.
+class User(AbstractUser):
+     # remove the username field
+    username = None
+
+    # add the phone_number field as the new unique identifier
+    phone_number = models.CharField(max_length=10, unique=True)
+
+    # add the email and national_id fields
+    email = models.EmailField(unique=True)
+    national_id = models.CharField(max_length=16,validators=[
+        RegexValidator(r'^[0-9]{16}$', 'Only 16-digit numeric values are allowed.')
+    ])
+
+    # set the phone_number field as the USERNAME_FIELD
+    USERNAME_FIELD = 'phone_number'
+
+    # make the email and national_id fields required
+    REQUIRED_FIELDS = ['email', 'national_id']
+    # set the CustomUserManager as the default manager
+    objects = CustomUserManager()
