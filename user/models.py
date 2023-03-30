9c78c29788809f439901306650ff06f839602b70
danielerat <davidodo2015@gmail.com>
2023-03-30 19:42:17 +0200

    Creating An address Model and Registering it to the admin section

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+# Creating the Address Model
+class Address(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    province = models.CharField(max_length=250, null=True)
+    district = models.CharField(max_length=250, null=True)
+    sector = models.CharField(max_length=250, null=True)
+    street_name = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return "{}@{}".format(self.sector, self.street_name)
