273adaaeffefed430c37b29f45129f04ce9b5eec
danielerat <davidodo2015@gmail.com>
2023-03-30 19:23:57 +0200

    First Commit for our custom User Model and Registration to the admin Section

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+admin.site.register(User)
