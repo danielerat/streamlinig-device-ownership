a43b804155cc34e406c76a8f1c17b22aa87c9b4e
danielerat <davidodo2015@gmail.com>
2023-03-30 20:02:40 +0200

    Adding a field to tell apart the account types of users

--- core/models.py
+++ core/models.py
@@ -10,6 +10,11 @@ from django.conf import settings
 
 from core.user_manager import CustomUserManager
 
+CHOICES_ACCOUNT_TYPE = (
+    ("p", "Personal"),
+    ("b", "Business"),
+)
+
 # Custom User Model that is Only Unique to this project.
 class User(AbstractUser):
      # remove the username field
@@ -20,6 +25,12 @@ class User(AbstractUser):
 
     # add the email and national_id fields
     email = models.EmailField(unique=True)
+
+    # Accound type for users
+    account_type = models.CharField(
+        choices=CHOICES_ACCOUNT_TYPE, default="p", max_length=2, null=True
+    )
+
     national_id = models.CharField(max_length=16,validators=[
         RegexValidator(r'^[0-9]{16}$', 'Only 16-digit numeric values are allowed.')
     ])
@@ -31,3 +42,5 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['email', 'national_id']
     # set the CustomUserManager as the default manager
     objects = CustomUserManager()
+
+
