5af486d291d47f56a9be30d6507465ad5d03cf21
danielerat <davidodo2015@gmail.com>
2023-04-03 22:04:09 +0200

    Adding the device to the admin section

--- stream/admin.py
+++ stream/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from stream.models import Device
 # Register your models here.
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "model",
+        "serial_number",
+        "mac_address",
+        "imei",
+        "price",
+        "category"
+    ]
+    list_per_page = 20
+    list_select_related = ["owner"]
+    ordering = ["name", "price", "category"]
+    search_fields = [
+        "name",
+        "model",
+        "serial_number",
+        "mac_address",
+        "imei",
+        "owner__first_name",
+        "owner__last_name",
+        "owner__national_id",
+        "owner__phone_number",
+    ]
+    list_filter = ["status", "quality", "created"]
