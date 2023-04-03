6f1602c3b8b4be6ebbe7ef8fd784299f6ee05420
danielerat <davidodo2015@gmail.com>
2023-04-03 10:21:46 +0200

    Adding Device Serializer

---
+++ stream/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from stream.models import Device, DeviceImage
+
+#Device Image Serializer
+class DeviceImageSerializer(ModelSerializer):
+    class Meta:
+        model = DeviceImage
+        fields = ['id','image']
+
+# Device serializer
+class DeviceSerializer(ModelSerializer):
+    images=DeviceImageSerializer()
+    class Meta:
+        model = Device
+        fields = ["id","name","model","serial_number","mac_address","imei","price","category","desc","quality","status","owner","images"]
+
