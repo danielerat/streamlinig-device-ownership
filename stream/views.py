517650c940fae1da9d4f44f091a81a010d9f2d40
danielerat <davidodo2015@gmail.com>
2023-04-03 23:07:45 +0200

    Adding Device Viewset

--- stream/views.py
+++ stream/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from stream.models import Device
+
+from stream.serializers import DeviceSerializer
+
+# --- Ninja Views---
+
+
+class DeviceViewset(ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
