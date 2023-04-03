d4353ac0cd17c820cef1733e70cdfcc10fb123e2
danielerat <davidodo2015@gmail.com>
2023-04-03 08:48:28 +0200

    Adding the validator for the image size

---
+++ stream/validators.py
@@ -0,0 +1,5 @@
+from django.core.exceptions import ValidationError
+def validate_file_size(file):
+    max_size_kb=1000
+    if file.size>max_size_kb * 1024:
+        raise ValidationError(f"Fles can not be large than {max_size_kb} KB")
