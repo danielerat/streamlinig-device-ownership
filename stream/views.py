fdcdbc7132ff320d0c01eb640917b896d59c3ce3
danielerat <davidodo2015@gmail.com>
2023-04-05 23:33:15 +0200

    Creating a custom action to transfer a device to a particular user

--- stream/views.py
+++ stream/views.py
@@ -4,14 +4,28 @@ from rest_framework.viewsets import ModelViewSet
 
 from stream.models import Device, DeviceImage
 
-from stream.serializers import DeviceImageSerializer, DeviceSerializer
+from stream.serializers import DeviceImageSerializer, DeviceSerializer, DeviceTransferSerializer
 
+from rest_framework.decorators import action
+
+from rest_framework.response import Response
 # --- Ninja Views---
 
 
 class DeviceViewset(ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+
+    @action(detail=True, methods=['post'])
+    def transfer(self, request, pk=None):
+        serializer = DeviceTransferSerializer(
+            data=request.data, context={"device_pk": self.kwargs['pk'], "request": self.request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # Add your custom logic here
+        # my_custom_data = serializer.validated_data['my_custom_field']
+
+        return Response("Ok Ok")
 
 
 class DeviceImageViewset(ModelViewSet):
@@ -22,4 +36,4 @@ class DeviceImageViewset(ModelViewSet):
 
     def get_serializer_context(self):
         # Get the image id and send it in the backend
-        return {"device_pk": self.kwargs["device_pk"]}
+        return {"device_pk": self.kwargs["device_pk"], "user": self.request.user}
