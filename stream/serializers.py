5c108dd473befdc79a435e7444e011634ceb3dd6
danielerat <davidodo2015@gmail.com>
2023-04-05 23:35:31 +0200

    Modifying the create method from device serializer to check whether a device is being added for the first time and add a record in the device first assignment

--- stream/serializers.py
+++ stream/serializers.py
@@ -1,10 +1,16 @@
-from rest_framework import serializers
+from django.conf import settings
+from rest_framework import serializers, status
 from rest_framework.serializers import ModelSerializer
 from core.serializers import SimpleUserSerializer
 from datetime import datetime, timedelta
 from django.utils import timezone
+from django.db import transaction
+import re
 
-from stream.models import Device, DeviceImage, Warranty
+from stream.models import Device, DeviceFirstAssignment, DeviceImage, PendingTransfer, Transfer, Warranty
+from core.models import User
+
+from rest_framework.exceptions import APIException
 
 # warranty serializer
 
@@ -41,10 +47,67 @@ class DeviceImageSerializer(ModelSerializer):
 
 class DeviceSerializer(ModelSerializer):
     images = DeviceImageSerializer(many=True, read_only=True)
-    owner = SimpleUserSerializer(read_only=True)
     warranty = WarrantySerializer(read_only=True)
 
     class Meta:
         model = Device
         fields = ["id", "name", "model", "serial_number", "mac_address", "imei",
                   "price", "category", "desc", "quality", "status", "owner", "images", "warranty"]
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        device, created = Device.objects.get_or_create(**validated_data)
+        # if device is not being saved for the first time, update the existing instance
+        if created:
+            device.save()
+            DeviceFirstAssignment.objects.create(
+                device_id=device.pk, holder=request.user, first_owner=device.owner)
+        return device
+
+
+class DeviceTransferSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        min_length=10, max_length=10, allow_blank=False)
+
+    national_id = serializers.CharField(
+        max_length=16)
+
+    def validate(self, data):
+        device_pk = self.context.get('device_pk')
+        user = self.context.get('request').user
+        if not re.match(r'^\d{16}$', data['national_id']):
+            raise serializers.ValidationError('Invalid National ID',
+                                              status_code=status.HTTP_400_BAD_REQUEST)
+        elif not re.match(r'^\d{10}$', data['phone_number']):
+            raise serializers.ValidationError('Phone Number is Invalid',
+                                              status_code=status.HTTP_400_BAD_REQUEST)
+        elif not Device.objects.filter(id=device_pk).exists():
+            raise serializers.ValidationError(
+                'An unknown Device is Being Transferred.', status_code=status.HTTP_400_BAD_REQUEST)
+        elif not User.objects.filter(phone_number=data['phone_number'], national_id=data['national_id']).exists():
+            raise serializers.ValidationError(
+                'You Are trying to transfer a device to an unknown User.')
+        elif not Device.objects.filter(id=device_pk, owner=user.id).exists():
+            raise serializers.ValidationError(
+                "You can not Transfer This device.")
+
+        return data
+
+        #     raise APIException("You can not transfer a device that does not belong to you",
+        #                        status_code=status.HTTP_401_UNAUTHORIZED)
+
+    def save(self, **kwargs):
+        device_pk = self.context.get('device_pk')
+        phone_number = self.validated_data['phone_number']
+        national_id = self.validated_data['national_id']
+        transferor = self.context.get('request').user
+        # If one fails then other fails too.
+        with transaction.atomic():
+            # We have a cart item, Update it.
+            device = Device.objects.get(id=device_pk)
+            owner = User.objects.get(
+                phone_number=phone_number, national_id=national_id)
+            PendingTransfer.objects.create(
+                device=device, transferor=transferor, transferee=owner)
+            self.instance = device
+        return self.instance
