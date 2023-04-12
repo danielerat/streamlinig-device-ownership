0ff27b8190e145b298198c7697b9f004dddfc9be
danielerat <davidodo2015@gmail.com>
2023-04-12 20:10:21 +0200

    Creating the Registration api view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import exceptions
 
-# Create your views here.
+
+from authentication.serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException("Password Do Not Match")
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
