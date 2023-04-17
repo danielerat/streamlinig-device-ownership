a46674d81767a294f8ab6d260d434eaee7df76dc
danielerat <davidodo2015@gmail.com>
2023-04-17 23:04:21 +0200

    Creating the send email mechanism for account creation

--- authentication/utils/send_email_templates.py
+++ authentication/utils/send_email_templates.py
@@ -2,17 +2,19 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
 
-def send_email(subject, to, template_name, context):
+
+def send_welcome_email(to, names):
     # Render the email template
-    email_template = render_to_string(template_name, context)
-    
+    email_template = render_to_string(
+        'emails/welcome_email.html', {'names': names})
+
     # Create an EmailMessage object
     email = EmailMessage(
-        subject,  # Subject of the email
+        'Welcome At Streamlining Device Tracking',  # Subject of the email
         email_template,  # Content of the email (the rendered template)
         settings.DEFAULT_FROM_EMAIL,  # From email address
         [to],  # List of recipient email addresses
     )
-    
+
     # Send the email
     email.send()
