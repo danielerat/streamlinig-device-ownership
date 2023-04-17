08a422bb89a255a3b83e011824831842df48eddf
danielerat <davidodo2015@gmail.com>
2023-04-17 16:19:30 +0200

    Registering other models in the admin section

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from authentication.models import User
+from authentication.models import User, Profile, Address, BusinessInfo, UserToken, Reset
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(BusinessInfo)
+admin.site.register(UserToken)
+admin.site.register(Reset)
