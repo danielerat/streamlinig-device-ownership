d5448c06721740c3c50d7deae01ed8f07c647c00
danielerat <davidodo2015@gmail.com>
2023-04-19 00:30:36 +0200

    Adding New Configurations of our database

--- streamlining/settings/prod.py
+++ streamlining/settings/prod.py
@@ -1,16 +1,20 @@
 from .common import *
 import os
-import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ['streamlining.herokuapp.com']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+DATABASE_URL = os.getenv('JAWSDB_URL')
 
-DATABASES = {
-    "default": dj_database_url.config()
-}
+database_attr = DATABASE_URL.split(':')
+
+JaName = database_attr[3].split('/')[1].rstrip("'")
+JaUser = database_attr[1].lstrip('//')
+JaPwrd = database_attr[2].split('@')[0]
+JaHost = database_attr[2].split('@')[1]
+JaPort = int(database_attr[3].split('/')[0])
 
 # Email Configurations
 EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
