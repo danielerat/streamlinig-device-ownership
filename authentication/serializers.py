bd21c3a165101ffff719c6c039b66299b9809106
danielerat <davidodo2015@gmail.com>
2023-04-23 22:47:41 +0200

    Fixing the arror, on user serializer, -Adding phone number and national_id on the user serialzier

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name',
-                  'email', 'password', 'devices']
+                  'email', 'phone_number', 'national_id', 'password', 'devices']
         extra_kwargs = {
             'password': {'write_only': True},
             'devices': {'read_only': True}
